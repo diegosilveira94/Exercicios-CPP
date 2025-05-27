ad9b867774a356fb284400139d51a7078ebc5588
Diego Silveira <diegocilveira@gmail.com>
2025-05-26 23:18:05 -0300

    Create 1_Vetor_10idades_Media.cpp

---
+++ 5ª LISTA/1_Vetor_10idades_Media.cpp
@@ -0,0 +1,22 @@
+/* 1. Leia um vetor com 10 idades. Calcule e imprima: a média das idades, a
+quantidade de pessoas com idade superior e inferior à média calculada
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+int vector[10];
+
+int main(){
+    srand(time(0)); // reseta o rand com a hora atual
+    system("chcp 65001");
+    system("cls");
+    cout << "\n\n\t =============================" << endl;
+    cout << "\t "
+    cout << "\n\n\t =============================" << endl;
+
+	cout<<"\n\n\n\n";
+	
+	return 0;
+}
